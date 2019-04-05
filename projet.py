866e21f0dc60d122ec33733dd9c439eb530171bf
pierre-marc laforest <pmlaforest@hotmail.com>
2019-04-04 23:39:34 -0400

    gitignore dataset

--- projet.py
+++ projet.py
@@ -1,4 +1,12 @@
 import pickle
-pickle_in = open("informationFile5.pickle","rb")
-test = pickle.load(pickle_in)
-print(test)
+
+def main():
+
+    pickle_in = open("informationFile5.pickle","rb")
+    test = pickle.load(pickle_in)
+    print(test)
+
+    return
+
+
+main()
